1b3f9d45614c3308ee7d4328888f0bbaa9a0ff24
ahmad-ben <ahmedbenchakhter@gmail.com>
2025-04-12 20:45:33 +0100

    Clean up files.

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-// const nav = document.querySelector(".nav"),
-//   searchIcon = document.querySelector("#searchIcon"),
-//   navOpenBtn = document.querySelector(".navOpenBtn"),
-//   navCloseBtn = document.querySelector(".navCloseBtn");
-// searchIcon.addEventListener("click", () => {
-//   nav.classList.toggle("openSearch");
-//   nav.classList.remove("openNav");
-//   if (nav.classList.contains("openSearch")) {
-//     return searchIcon.classList.replace("uil-search", "uil-times");
-//   }
-//   searchIcon.classList.replace("uil-times", "uil-search");
-// });
-// navOpenBtn.addEventListener("click", () => {
-//   nav.classList.add("openNav");
-//   nav.classList.remove("openSearch");
-//   searchIcon.classList.replace("uil-times", "uil-search");
-// });
-// navCloseBtn.addEventListener("click", () => {
-//   nav.classList.remove("openNav");
-// });
-
 const topNav = document.querySelector(".nav-links");
 const menuBtn = document.querySelector(".menu-btn");
-const closeNavBtn = document.querySelector(".close-btn");
-const sectionsHref = document.querySelectorAll(".section-href");
 
 menuBtn.addEventListener("click", () => {
   topNav.classList.add("show")
@@ -31,10 +8,3 @@ menuBtn.addEventListener("click", () => {
 topNav.addEventListener("click", () => {
   topNav.classList.remove("show")
 });
-
-
-// sectionsHref.forEach((sectionHref) => {
-//   sectionHref.addEventListener("click", () => {
-//     topNav.classList.remove("show")
-//   });
-// })
